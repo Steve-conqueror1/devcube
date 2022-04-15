76e038d4cc8f364394b2c0ae84fd5bb5c2d08776
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2022-04-15 14:19:05 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const readline = require('readline');
+const fs = require('fs');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function input(prompt) {
+  return new Promise((callbackFn, errorFn) => {
+    rl.question(
+      prompt,
+      (uinput) => {
+        callbackFn(uinput);
+      },
+      () => {
+        errorFn();
+      }
+    );
+  });
+}
+
+const params = {};
+
+const setParams = async () => {
+  params.filename = await input('o- ');
+  params.ids = (await input('p- ')).split('');
+  rl.close();
+};
+
+const writeToJSON = (path, content) => {
+  fs.appendFile(path, JSON.stringify(content), (err) => {
+    console.log('Error occured while trying to write to the file', err);
+  });
+};
+
+const getRequiredData = (dataObj) => {
+  const { id, name, description } = dataObj;
+  return { id, name, description };
+};
+
+const getProjects = async () => {
+  await setParams();
+  const path = `./${params.filename}.json`;
+
+  for (const id of params.ids) {
+    try {
+      const data = await axios.get(`${process.env.API_URL}/projects/${id}`, {
+        headers: {
+          'Shortcut-Token': process.env.SHORTCUT_API_TOKEN,
+        },
+      });
+
+      const { data: projects } = data;
+      const copiedProjFilelds = getRequiredData(projects);
+      writeToJSON(path, copiedProjFilelds);
+    } catch (e) {
+      console.log('Erorr Occured--', e.message);
+    }
+  }
+};
+
+getProjects();
